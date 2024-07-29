1f6d9b5385f8055158ca60d8e3c1da1969a9ed72
ppkoch <kochpatrick@me.com>
2024-07-29 16:58:11 +0200

    Update element.js

--- src/element.js
+++ src/element.js
@@ -1,6 +1,6 @@
 //# sourceMappingURL=element.js.map
 
-console.log('test');
+console.log('test1');
 (function(e,o){if("object"===typeof exports&&"object"===typeof module)module.exports=o();else if("function"===typeof define&&define.amd)define([],o);else{var t=o();for(var n in t)("object"===typeof exports?exports:e)[n]=t[n]}})(self,(function(){return function(){var e={};return console.log("element.js geladen"),e}()}));
 //# sourceMappingURL=element.js.map
 console.log('test2');
